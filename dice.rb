bf5445b5bf591d5fb5a25772dd5ae7ab13f3b92f
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-18 03:20:33 +0000

    Added debugging helpers.

--- dice.rb
+++ dice.rb
@@ -1,6 +1,12 @@
 # /dice.rb
 
 require "sinatra"
+require "better_errors"
+require "binding_of_caller"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "Hello World"
